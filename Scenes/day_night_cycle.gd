c8eb88b950a97d0dad70c260646fc689a23ab767
Kaede <kaedecarreras@gmail.com>
2026-07-01 13:58:33 -0400

    tree base

--- Scenes/day_night_cycle.gd
+++ Scenes/day_night_cycle.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var time : float
-@export var day_length : float = 20.0
+@export var day_length : float = 200.0
 @export var start_time : float = 0.3
 var time_rate : float
 
@@ -9,12 +9,13 @@ var time_rate : float
 var sun : DirectionalLight2D
 @export var sun_color : Gradient 
 @export var sun_intensity : Curve
-var sunSprite : Sprite2D
-	
+var sunSprite : AnimatedSprite2D
+#var sunLight : PointLight2D
 # moon
 var moon : DirectionalLight2D
 @export var moon_color : Gradient 
 @export var moon_intensity : Curve
+var moonLight : PointLight2D
 
 # sky (this didn't work, but leaving it here in case) 
 var environment : ColorRect
@@ -40,6 +41,7 @@ var environment : ColorRect
 # sun/moon follow spline
 @onready var sun_follow: PathFollow2D = get_node("CanvasLayer/sunPath/sunFollow")
 @onready var moon_follow: PathFollow2D = get_node("CanvasLayer/moonPath/moonFollow")
+@onready var sunLight : PointLight2D = get_node("CanvasLayer/sunPath/sunFollow/sunSprite/sunLight")
 
 # sun/moon sprite sky relativity config 
 @export var sky_center : Vector2 = Vector2(300, 90) 
@@ -55,6 +57,7 @@ var environment : ColorRect
 @export_range(0.0, 1.0) var sunset_start  := 0.62
 @export_range(0.0, 1.0) var sunset_end    := 0.78
 
+
 var tween : Tween
 # ===========================================
 
@@ -63,6 +66,10 @@ var tween : Tween
 var yearDay : int # for tracking season progression
 var totalDay : int # for tracking total days survived
 # ===========================================
+
+# WEATHER
+@export var yearWeatherChance : Curve
+var weatherChanceMultiplier : float = 0.001
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -78,6 +85,8 @@ func _ready() -> void:
 	sunset_rect.modulate.a = 0.0
 	night_rect.modulate.a = 1.0
 	_update_sky(time)
+	
+	sun_sprite.play("shine")
 func set_sky(texture: Texture2D, fade_time : float = 1.0) -> void:
 	var back := night_rect
 	var front := day_rect # assume day_rect is the front one initially 
@@ -145,6 +154,18 @@ func _arc_pos(t: float, center_angle_deg: float, span_deg: float) -> Vector2:
 	print(angle)
 	return sky_center + Vector2(cos(angle) * sky_radius.x, -sin(angle) * sky_radius.y)	
 
+# WEATHER FUNCTIONS
+# ==========================================================================
+func weatherChance(yearlyChance: float, weatherChanceMultiplier: float):
+	var rng = RandomNumberGenerator.new()
+	var random_value = rng.randf_range(0, 1) * weatherChanceMultiplier
+	if random_value >= yearlyChance :
+		print("rain today")
+		return true
+	else: 
+		return false
+# ==========================================================================
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	time += time_rate * delta
@@ -154,7 +175,8 @@ func _process(delta: float) -> void:
 	sun.energy = sun_intensity.sample(time)
 	sun.rotation_degrees = time * 360 + 90 	# rotate sun
 	sun.visible = sun.energy > 0
-
+	sunLight.color = sun_color.sample(time)
+	
 	# moon stuff
 	moon.color = moon_color.sample(time)
 	moon.energy = moon_intensity.sample(time)
@@ -170,8 +192,8 @@ func _process(delta: float) -> void:
 		time = 0.0
 		
 		# season tracking
-		yearDay = yearDay + 1
-		if yearDay > 365:
+		yearDay = (yearDay + 1) / 365 # normalize 0..1
+		if yearDay > 1:
 			yearDay = 0
 			
 	totalDay = totalDay + 1 # add to total days survived counter
@@ -185,13 +207,13 @@ func _process(delta: float) -> void:
 	#if(time > sunrise_start && time < sunset_end) :
 	sun_follow.progress_ratio = (1-time-0.25)
 	#else :
-	moon_follow.progress_ratio = fract((1-time) + 0.5) # opposite phase
+	moon_follow.progress_ratio = fract((1-time) + 0.5-0.25) # opposite phase
 
 	# simple "below horizon" hide
 	#var sun_t := (sun_pos.y - sky_center.y) / sky_radius.y
 	#var moon_t := (moon_pos.y - sky_center.y) / moon_radius.y
 	#sun_sprite.visible = sun_t > 0.0
 	#moon_sprite.visible = moon_t > 0.0
-
+	weatherChance(yearWeatherChance.sample(yearDay),weatherChanceMultiplier) # calculate chance of weather event
 
 	_update_sky(time)
